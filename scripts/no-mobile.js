fdb16a88de66c203d4c9ebbfaeebb3971631e1a8
JdM <julien@rmbi.ch>
2024-11-05 09:12:46 +0100

    Update no-mobile.js

--- scripts/no-mobile.js
+++ scripts/no-mobile.js
@@ -1,4 +1,4 @@
-function isMobileOrSmallScreen() {
+/* function isMobileOrSmallScreen() {
     const mobileBreakpoint = 768; // Définir un point de rupture pour les petits écrans
     return /Mobi|Android/i.test(navigator.userAgent) || window.innerWidth < mobileBreakpoint;
 }
@@ -6,3 +6,4 @@ function isMobileOrSmallScreen() {
 if (isMobileOrSmallScreen()) {
     document.getElementById('mobile-warning').style.display = 'flex';
 }
+*/
