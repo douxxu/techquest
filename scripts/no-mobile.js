bac5b8d1eef412bd5efe4c5e08b601f0f9d65a11
Douxx <136928935+douxxu@users.noreply.github.com>
2024-11-05 16:15:55 +0100

    as

--- scripts/no-mobile.js
+++ scripts/no-mobile.js
@@ -1,9 +1,8 @@
-/* function isMobileOrSmallScreen() {
-    const mobileBreakpoint = 768; // Définir un point de rupture pour les petits écrans
+function isMobileOrSmallScreen() {
+    const mobileBreakpoint = 768;
     return /Mobi|Android/i.test(navigator.userAgent) || window.innerWidth < mobileBreakpoint;
 }
 
 if (isMobileOrSmallScreen()) {
     document.getElementById('mobile-warning').style.display = 'flex';
 }
-*/
