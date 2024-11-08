31dddb9021cc5e33ecf0a17d0a6e78a3ce16a6cf
douxxu <isaiaplacii@gmail.com>
2024-11-08 14:58:02 +0100

    Imported qr codes of techquest.zip

--- README.md
+++
@@ -1,7 +0,0 @@
-# TechQuest.work
-School project  
-  
-[Website link](https://techquest.work)  
-[Project doc](DOC.md)
-
-Licensed under GPL-3.0
--- scripts/no-mobile.js
+++
@@ -1,8 +0,0 @@
-function isMobileOrSmallScreen() {
-    const mobileBreakpoint = 768;
-    return /Mobi|Android/i.test(navigator.userAgent) || window.innerWidth < mobileBreakpoint;
-}
-
-if (isMobileOrSmallScreen()) {
-    document.getElementById('mobile-warning').style.display = 'flex';
-}
